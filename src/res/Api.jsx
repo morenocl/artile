ab05316b3bd306cef675bc9a9a42db6034fa9bd1
Christian L Moreno <christianluciano.m@gmail.com>
2020-04-11 17:09:53 -0300

    Implementacion boton de configuracion y formulario para ingresar productos.

--- src/res/Api.jsx
+++ src/res/Api.jsx
@@ -1,21 +1,28 @@
-export const path = process.env.REACT_APP_PATH || 'http://localhost:8000';
+//export const path = process.env.REACT_APP_PATH || 'http://localhost:8000';
+export const path = 'http://127.0.0.1:8000/art';
 
 export const request = (url, opts, onSuccess, onFailure) => {
-  const options = { ...opts };
+  const body = {
+    ...opts,
+    headers:{'Content-Type': 'application/json'},
+    mode: 'cors',
+  };
   // Add headers.
-  if (!options.headers) options.headers = {};
+//  if (!options.headers) options.headers = {};
   // Always use json.
-  options.headers['Content-Type'] = 'application/json';
+//  options.headers['Content-Type'] = 'application/json';
   // Add token if logged in.
   const token = "getToken()";
-  if (token) options.headers.Authorization = `Token ${token}`;
-  fetch(url, options)
+//  if (token) options.headers.Authorization = `Token ${token}`;
+  console.log(JSON.stringify(body));
+  fetch(url, {body})
     .then((r) => {
       if (!r.ok) throw Error(r.statusText);
       return r.json();
     })
     .then(onSuccess)
     .catch(onFailure);
+//  fetch(url, options);
 };
 
 export const getInicio = (onSuccess, onFailure) => {
@@ -70,3 +77,12 @@ export const login = (username, password, onSuccess, onFailure) => {
 
   request(url, options, onSuccess, onFailure);
 };
+
+export const postProducto = (data, onSuccess, onFailure) => {
+  const url = `${path}/productos`;
+  const body = {
+    method: 'POST',
+    body: data,
+  };
+  request(url, body, onSuccess, onFailure);
+}
