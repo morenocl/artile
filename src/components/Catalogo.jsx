acf15d437eb27e2fc93535c0d0eb4eb41f07ef38
Christian L Moreno <christianluciano.m@gmail.com>
2020-04-11 20:00:37 -0300

    Tabs para categorias.

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -9,55 +9,29 @@ import Tabs from 'react-bootstrap/Tabs'
 import '../res/css/catalogo.css'
 /*  style={{ width: '12rem' }} */
 const Catalogo = (props) => {
-  const { data } = props;
+  const { categ, mapeoCard } = props;
 
-  const mapeo = ({title, text, button, img, categ}, i, tab) => {
-    console.log(categ, tab);
-    if (categ === tab)
-      return(<Col xs={4} lg={3}>
-        <Card key={i} >
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>
-              {text}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">{button}</small>
-          </Card.Footer>
-        </Card></Col>
-      )
-  }
+  const taby = [];
 
-  const tabs = (
+  for (const p in categ){
+    taby.push(
+      <Tab eventKey={categ[p].key} title={categ[p].title} key={p}>
+        <CardDeck>
+          { categ[p].elems.map((dato, i) => {return (mapeoCard(dato, i))}) }
+        </CardDeck>
+      </Tab>
+    )
+  };
+
+  const tab = (
     <Tabs defaultActiveKey="guagua" id="uncontrolled-tab-example">
-      <Tab eventKey="guagua" title="Artile Guagua">
-        <CardDeck>
-          { data.map((dato, i) => {return (mapeo(dato, i, 'guagua'))}) }
-        </CardDeck>
-      </Tab>
-      <Tab eventKey="natural" title="Artile Natural">
-        <CardDeck>
-          { data.map((dato, i) => {return (mapeo(dato, i, 'natural'))}) }
-        </CardDeck>
-      </Tab>
-      <Tab eventKey="textil" title="Artile Labores Textiles">
-        <CardDeck>
-          { data.map((dato, i) => {return (mapeo(dato, i, 'textil'))}) }
-        </CardDeck>
-      </Tab>
-      <Tab eventKey="kutxa" title="Artile Mimos & Regalos">
-        <CardDeck>
-          { data.map((dato, i) => {return (mapeo(dato, i, 'kutxa'))}) }
-        </CardDeck>
-      </Tab>
+      {taby.map((t, i) => {return(t)})}
     </Tabs>
-  )
+  );
 
   return(<>
     <h1>Catalogo</h1>
-    {tabs}
+    {tab}
     </>
   )
 }
--- src/containers/Catalogo.jsx
+++ src/containers/Catalogo.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Col from 'react-bootstrap/Col'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
 import CatalogoScreen from '../components/Catalogo'
 import OfertasScreen from '../components/Ofertas'
 import { productos, ofertas } from '../res/Datos'
 
+
 const Catalogo = (props) => {
+
+  const categ = {
+    guagua:{title:"Artile Guagua", key:"guagua" , elems:[],},
+    natural:{title:"Artile Natural", key:"natural" , elems:[],},
+    textil:{title:"Artile Textil", key:"textil" , elems:[],},
+    kutxa:{title:"Artile Kutxa", key:"kutxa" , elems:[],},
+  };
+
+  productos.forEach(dato => categ[dato.categ].elems.push(dato));
+
+  const mapeoCard = ({title, text, button, img, categ}, i) => {
+    return(
+      <Col xs={4} lg={3} key={i}>
+        <Card key={i} >
+          <Card.Img variant="top" src={img} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+              {text}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">{button}</small>
+          </Card.Footer>
+        </Card>
+      </Col>
+    )
+  }
+
 
   return(
     props.type === 'ofertas' ?
       <OfertasScreen data={ofertas}/>
       :
-      <CatalogoScreen data={productos}/>
+      <CatalogoScreen categ={categ} mapeoCard={mapeoCard}/>
   );
 }
 
