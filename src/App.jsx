76959d7a7009f1d2ba7e8a8b405148d0b5e16c17
Christian L Moreno <christianluciano.m@gmail.com>
2020-05-10 16:49:57 -0300

    Arreglado pie de pagina.

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,24 @@ import {NavCol} from './containers/NavCol';
 import Main from './containers/Main';
 import Pie from './containers/Pie';
 
+import './res/css/style.css'
+
 const App = () => {
   return(
-    <div className="App">
-      <NavBar />
-      <Row>
-        <Col xs={12} sm={3} md={2} lg={1}>
-          <NavCol />
-        </Col>
-        <Col xs={12} sm={9} md={10} lg={11}>
-          <Main />
-        </Col>
-      </Row>
+    <>
+      <div className="App" id='page-container'>
+        <NavBar />
+        <Row>
+          <Col xs={12} sm={3} md={2} lg={1}>
+            <NavCol />
+          </Col>
+          <Col xs={12} sm={9} md={10} lg={11}>
+            <Main />
+          </Col>
+        </Row>
+      </div>
       <Pie />
-    </div>
+    </>
   )
 }
 
