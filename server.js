5db10eb06743a3985f29ae928d2e1e28d1c72095
Christian L Moreno <christianluciano.m@gmail.com>
2020-05-12 01:59:41 -0300

    Revierto commit anterior.

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-var env = process.env.NODE_ENV || 'development';
-if ('development' == env) {
-  app.use(express.static(__dirname+'/public')); // Catch static files
-  app.use(function(req, res, next) {
-    if (req.url == '/view') {
-      next();
-    } else {
-      res.redirect('/index.html');
-    }
-  });
-};
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
